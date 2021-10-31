8554183fa28e8906d05ddd73c46de233b96f7410
Patrik Larsen <patrik@patriklarsen.com>
2021-10-31 03:39:34 +0200

    improved comments

--- W01E01_PushLED.X/main.c
+++ W01E01_PushLED.X/main.c
@@ -1,6 +1,9 @@
 /*
  * File:   main.c
- * Author: Patrik Larsen
+ * Author: Patrik Larsen <pslars@utu.fi>
+ * Exercise: W01E01 - Push-button Light
+ * Description: Turns on and off the built-in light at the push
+ * of a button.
  *
  * Created on October 29, 2021, 6:52 PM
  */
@@ -8,19 +11,22 @@
 
 #include <avr/io.h>
 int main(void) {
+
+    //LED pin set as output
+    PORTF.DIR = (PORTF.DIR | PIN5_bm);
     
-    //LED as output
-    PORTF.DIR = PORTF.DIR | PIN5_bm;
-    
-    //BUTTON as input
-    PORTF.DIR = PORTF.DIR & ~PIN6_bm;
+    //BUTTON pin set as input
+    PORTF.DIR = (PORTF.DIR & ~PIN6_bm);
 
     while (1) {
-        if (PORTF.IN & PIN6_bm){
-            // If button is not pressed
+        // If button is not pressed
+        if (PORTF.IN & PIN6_bm)
+        {
             PORTF.OUT |= PIN5_bm; //led is turned off
-        } else {
-            // if the button is pressed
+        }
+        // if the button is pressed
+        else
+        {
             PORTF.OUT &= ~PIN5_bm; //led is turned on
         }
     }
